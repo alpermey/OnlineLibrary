e814088af28844a68da1322c546599d59d7bd9bc
alpermey <alperm@moonadmins-MacBook-Pro.local>
2020-11-06 02:57:06 +0300

    Bookspage:Created a increase right click functionality

--- src/app/bookspage/bookspage.component.ts
+++ src/app/bookspage/bookspage.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Book } from 'src/book.component';
 import { DialogComponent } from './dialog/dialog.component';
+import {cloneDeep} from 'lodash';
 
 @Component({
   selector: 'app-bookspage',
@@ -11,19 +12,23 @@ import { DialogComponent } from './dialog/dialog.component';
 
 export class BookspageComponent implements OnInit {
 
-  name: ''; 
-  nameOfBooks: string[];
-  books: Book[] = [];
-
   constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {}
   
-  typesOfBooks: string[] = ['War and Peace', 'Crime and Punishment', 'The Cherry Orchard'];
+  b: Book[] = [{name:'War and Peace',author:'Tolstoy',amount:5,id:0},
+  {name:'Crime and Punishment',author:'Dostoevsky',amount:3,id:1}]
 
-  b: Book[] = [{name:'W',author:'wathor',amount:5,id:0},
-  {name:'A',author:'author',amount:3,id:1}
-]
+  clonedArray = cloneDeep(this.b);; 
+
+  onRightClick(id: number){
+    const sum = this.clonedArray[id].amount;
+    console.log(sum);
+    if(sum != this.b[id].amount){
+      this.b[id].amount= this.b[id].amount + 1;
+    }
+    return false;
+  }
 
   choose(id: number) {
     if(this.b[id].amount > 0){
